e1e4a5313df5db40369ce3afda9afc41337fc19d
Xiaomin Liu <xl4624@nyu.edu>
2024-12-03 13:53:49 -0500

    Fix hyprland.conf shadows

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -143,7 +143,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
-vim.g.python3_host_prog = '/usr/bin/python'
+vim.g.python3_host_prog = '/usr/bin/python3'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
