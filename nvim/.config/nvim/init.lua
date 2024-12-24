d1b0f75882ed124c4842856d9220bee99d91f48d
Xiaomin Liu <xl4624@nyu.edu>
2024-12-24 18:05:02 -0500

    Change Hyprland cursor and nvim logo

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,83 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -97,6 +17,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Set the default tabwidth to 4
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
@@ -334,10 +255,10 @@ require('lazy').setup({
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
+      local ignore_patterns = { 'node_modules', '.git', '.venv', 'target', 'dist', '.cache', '*.o' }
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
-        --
         defaults = {
           mappings = {
             n = { ['<c-s>'] = 'select_horizontal', ['<c-x>'] = 'delete_buffer' },
@@ -347,13 +268,15 @@ require('lazy').setup({
         },
         pickers = {
           live_grep = {
-            file_ignore_patterns = { 'node_modules', '.git', '.venv', 'target' },
-            additional_args = function(_)
-              return { '--hidden', '--no-ignore' }
-            end,
+            file_ignore_patterns = ignore_patterns,
+            hidden = true,
+            no_ignore = true,
+            -- additional_args = function(_)
+            --   return { '--hidden', '--no-ignore' }
+            -- end,
           },
           find_files = {
-            file_ignore_patterns = { 'node_modules', '.git', '.venv', 'target' },
+            file_ignore_patterns = ignore_patterns,
             hidden = true,
             no_ignore = true,
           },
@@ -915,6 +838,23 @@ require('lazy').setup({
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
+  },
+
+  -- Disable certain features like LSP and treesitter on big files.
+  {
+    'LunarVim/bigfile.nvim',
+    opts = {
+      -- Features to disable.
+      features = {
+        -- 'indent_blankline',
+        -- 'illuminate',
+        'lsp',
+        'treesitter',
+        'syntax',
+        -- 'vimopts',
+        -- 'filetype',
+      },
+    },
   },
 
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
